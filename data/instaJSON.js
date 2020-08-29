0d92c246dbd3f9323265d05c87ba2c7d385fabdc
Herve-Daniel Barihuta <d.barihuta@gmail.com>
2020-08-29 22:40:24 +0800

    Update to insta embed

---
+++ data/instaJSON.js
@@ -0,0 +1,28 @@
+{
+    "posts": [{
+        "src": "media/posts/117756434_169801161421212_2417533353158951543_n.jpg",
+        "url": "https://www.instagram.com/p/CEEdB_GHv62/",
+        "caption": "Awesome ACCELERATION session for our Wednesday Warriors. Exposed the group to a few new movements to activate further progress. 'Good is not good enough when BETTER is expected'. #Youthtraining #sprintacceleration",
+        "likes": "20",
+        "comments": "0"
+        }, {
+        "src": "media/posts/117228795_600421380671749_2380925154539869418_n.jpg",
+        "url": "https://www.instagram.com/p/CDv0wuBHJI2/",
+        "caption": "Grateful for the opportunity to conduct a SAQ session with @northcoastrufc under 15s. Boys were eager to learn and were engaged throughout the session. Coach Junior Aniterea has done an amazing job putting these boys together and creating a great work ethic in the club. Thankful to be involved with a great club.",
+        "likes": "18",
+        "comments": "0"
+        }, {
+        "src": "media/posts/117109331_297911468310967_5992733996753156441_n.jpg",
+        "url": "https://www.instagram.com/p/CDnI7vYHNLH/",
+        "caption": "Was great to come down and watch some of our Linespeed athletes in action. Hunter, Harley and Eli put on a dominant performance for their @northcoastrugbyunionfootball under 9s. Welldone boys. Was awesome to witness. #Rugbywa #northcoastrugbyunion",
+        "likes": "29",
+        "comments": "0"
+        }, {
+        "src": "media/posts/117318807_128464075604947_2938130613125404026_n.jpg",
+        "url": "https://www.instagram.com/p/CDnI7vYHNLH/",
+        "caption": "Linespeed AFFIRMATIONs 4 words/pillars that Linespeed athletes use to prepare themselves for optimal performance. ✅ GRATITUDE (Respect factor) To be present in the now To see positives in the current moment To appreciate what athletes have available to them ✅ INSPIRATION (Why factor) Who inspires you Why do they inspire you How did they achieve their goals Who do you think you inspire ✅ SELF BELIEF (Resilience factor) Envision your best result Trust yourself to be better Do not let anyone/anything distract you from being your best ✅ EXCELLENCE (Discipline factor) What does our best look like Do I strive for excellence Do I give it my best You decide that nothing else but my best will be given 70percent of athlete performance is in mental preparedness. If we can believe, we can achieve. Linespeed AHEAD. #affirmations #GRATITUDE #INSPIRE #SELFBELIEF #EXCELLENCE",
+        "likes": "12",
+        "comments": "0"
+        }
+    ]
+}
